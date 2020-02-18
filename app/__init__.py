dde58f030f3712e222269df096d4e6c29880f355
Marionjames-ally <marionjames2018@gmail.com>
2020-02-18 15:21:11 +0300

    register blueprints && set up configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from config import config_options
 
-def create_app():
+def create_app(config_name):
     app= Flask(__name__)
+    # setting configurations
+    app.config.from_object(config_options[config_name])
+
+    # registering blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
 
     return app
