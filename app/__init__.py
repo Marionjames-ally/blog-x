d7c4b20f0517ec2ed195386f3346d20e31b2a1c0
Marionjames-ally <marionjames2018@gmail.com>
2020-02-18 15:47:22 +0300

    setup request config

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ def create_app(config_name):
     app= Flask(__name__)
     # setting configurations
     app.config.from_object(config_options[config_name])
+    from .requests import configure_request
+    configure_request(app)
 
     # registering blueprints
     from .main import main as main_blueprint
