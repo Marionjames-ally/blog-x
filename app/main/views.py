54a5493680e7b459456a754cf2fc4f0b114cf331
Marionjames-ally <marionjames2018@gmail.com>
2020-02-19 09:56:11 +0300

    display quotes on the screen

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,28 @@
 from ..models import Quotes
-from flask import render_template,request,redirect,url_for, flash
-from ..models import User, Blogs, Comment, Subscriber   
-from flask_login import login_required,current_user
+from flask import render_template, request, redirect, url_for, flash
+from ..models import User, Blogs, Comment, Subscriber
+from flask_login import login_required, current_user
 from . import main
 from ..request import get_quotes
 
+
 @main.route('/')
 def index():
-    '''
-    view quotes page that returns the quotes data
-    '''
+    """
+    returns the index page and its data
+    :return:
+    """
+    title = 'HOME'
     quotes = get_quotes()
-    render_template('index.html', quotes =quotes)
+    return render_template('index.html', title=title, quotes=quotes)
+
+
+@main.route('/quotes/<int:id>')
+def quotes():
+    """
+    view quotes and its data
+    :return:
+    """
+    quote = get_quotes(id)
+    title = 'HOME OF QUOTES'
+    return render_template('quotes.html', quote=quote, title=title)
